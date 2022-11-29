4224d193a18909da67a9f2927f66db5ba71c403c
Sami Kuikka <sami.kuikka@aalto.fi>
2022-11-29 15:42:53 +0200

    test commit

--- development/online_education_app_rn_student/App.js
+++ development/online_education_app_rn_student/App.js
@@ -75,10 +75,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
+          <Section title="Step One">not</Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
