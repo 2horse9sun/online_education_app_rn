7c2e1098c5b36b4d77f5e0452ba80c06e08eb732
Sami Kuikka <sami.kuikka@aalto.fi>
2022-12-01 19:36:19 +0200

    deleted unecessary imports

--- development/online_education_app_rn_student/src/pages/MyProfile.js
+++ development/online_education_app_rn_student/src/pages/MyProfile.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import { Image, StyleSheet, TouchableOpacity, View, ScrollView, RefreshControl, ActivityIndicator, ImageBackground } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Card, Title, Paragraph, ProgressBar, Colors, Button, DefaultTheme, Snackbar, Text, Appbar } from 'react-native-paper';
-import { truncate } from '../utils/StringUtil';
-import Entypo from 'react-native-vector-icons/Entypo';
-import { formatDate } from '../utils/DateUtil';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+
 
 
 const MyProfilePage = () => {
